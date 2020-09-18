1a8f545955d5c13d55527293fe97cbc55997d224
PRAY <wuwenbang@foxmail.com>
2020-09-18 15:50:50 +0800

    feat: 🎸 完成图标环绕样式

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import React from "react";
-import "./App.scss";
+import React from 'react'
+import './App.scss'
 function App() {
   return (
     <div className="wrapper">
-      <div className="search">
+      <form className="search" id="myForm" action="http://www.baidu.com/s" target="_blank">
         <div className="search-icon">
           <svg className="icon" aria-hidden="true">
             <use xlinkHref="#icon-baidu"></use>
           </svg>
         </div>
-        <input className="search-input" />
+        <input
+          className="search-input"
+          name="wd"
+          placeholder="请输入搜索内容"
+          autoComplete="on"
+          autoFocus="autofocus"
+          maxLength="255"
+        />
         <div className="search-button">
+          <input className="search-submit" type="submit"></input>
           <svg className="icon" aria-hidden="true">
             <use xlinkHref="#icon-search"></use>
           </svg>
         </div>
+      </form>
+      <div className="star-wrapper">
+        <div className="star-surround">
+          <svg className="icon" aria-hidden="true" className="star">
+            <use xlinkHref="#icon-earth"></use>
+          </svg>
+          <svg className="icon" aria-hidden="true" className="cat">
+            <use xlinkHref="#icon-cat"></use>
+          </svg>
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
