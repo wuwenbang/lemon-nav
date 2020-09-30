97665f9334977efbef6b82b5a1892da89c693192
PRAY <wuwenbang@foxmail.com>
2020-09-30 10:38:25 +0800

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,8 @@ cd build &&
 git init  &&
 git add . &&
 git commit -m 'deploy-update' &&
-git remote add origin git@github.com:wuwenbang/lemon-money-react-website.git &&
-git remote add gitee git@gitee.com:wuwenbang/lemon-money-react-website.git &&
+git remote add origin git@github.com:wuwenbang/lemon-nav-website.git &&
+git remote add gitee git@gitee.com:wuwenbang/lemon-nav-website.git &&
 git push -u origin master -f
 git push gitee -f
 cd -
