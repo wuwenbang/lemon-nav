8186fd071b4f3945074eca6d0840bd013312593c
PRAY <wuwenbang@foxmail.com>
2020-10-10 17:45:23 +0800

    更新壁纸设置样式和功能

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import './App.scss'
 import useBookmark from '../hooks/useBookmark'
+import defaultImage from '../assets/default.jpg'
+import image1 from '../assets/image1.jpg'
 function App() {
   const [settingFlag, setSettingFlag] = useState(false)
   const { bookmarks, createBookmark, deleteBookmark } = useBookmark()
@@ -8,6 +10,22 @@ function App() {
     let image = window.localStorage.getItem('image') || 'https://blog.mrabit.com/bing/today'
     document.querySelector('.bg').src = image
   }, [])
+  useEffect(() => {
+    document.querySelector('body').addEventListener('click', (e) => {
+      if (e.target.tagName !== 'use' && e.target.tagName !== 'svg') {
+        setSettingFlag(false)
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    const window = document.querySelector('.setting-window')
+    if (settingFlag) {
+      window.classList.remove('disappear')
+    } else {
+      window.classList.add('disappear')
+    }
+  }, [settingFlag])
   const onDeleteBookmark = (e) => {
     deleteBookmark(parseInt(e.currentTarget.id))
   }
@@ -33,28 +51,35 @@ function App() {
   // const onDragEnter = (e) => {
   //   e.preventDefault()
   // }
-  const onSettingOpen = () => {
-    setSettingFlag(true)
+  const onSettingToggle = () => {
+    setSettingFlag((flag) => !flag)
   }
-  const onSettingClose = () => {
-    setSettingFlag(false)
-  }
+
   const onImportFile = () => {
-    let selectedFile = document.querySelector('.importFile').files[0]
+    let selectedFile = document.querySelector('.import-file').files[0]
     let reader = new FileReader()
     reader.readAsDataURL(selectedFile)
     reader.onload = function () {
-      document.querySelector('.importImage').src = this.result
+      document.querySelector('.import-image').src = this.result
       document.querySelector('.bg').src = this.result
       window.localStorage.setItem('image', this.result)
     }
+  }
+  const onClickImportFile = () => {
+    document.querySelector('.import-file').click()
+  }
+  const onSettingBgClose = () => {
+    document.querySelector('.setting-background').classList.add('disappear')
+  }
+  const onSettingBgOpen = () => {
+    document.querySelector('.setting-background').classList.remove('disappear')
   }
   return (
     <div className="wrapper">
       {/* <video autoPlay loop muted>
         <source src="?" type="video/mp4" />
       </video> */}
-      <img src="https://blog.mrabit.com/bing/today" className="bg"></img>
+      <img src="https://blog.mrabit.com/bing/today" className="bg" alt="" />
       <form className="search" id="myForm" action="http://www.baidu.com/s" target="_blank">
         <div className="search-icon">
           <svg className="icon" aria-hidden="true">
@@ -107,36 +132,49 @@ function App() {
           </a>
         </li>
       </ul>
-      <div className="setting-btn" onClick={onSettingOpen}>
+      <div className="setting-btn" onClick={onSettingToggle}>
         <svg className="icon" aria-hidden="true">
           <use xlinkHref="#icon-setting"></use>
         </svg>
       </div>
-      {settingFlag ? (
-        <div className="setting-window">
-          <header className="setting-window-topbar">
-            <div className="setting-window-icon"></div>
-            <div className="setting-window-title">设置</div>
-            <div className="setting-window-close">
-              <svg className="icon " aria-hidden="true" onClick={onSettingClose}>
-                <use xlinkHref="#icon-close"></use>
-              </svg>
-            </div>
-          </header>
-          <main className="setting-window-main">
-            <div className="setting-window-sider">
-              <div className="setting-window-item">背景</div>
-              <div className="setting-window-item">关于</div>
-            </div>
-            <div className="setting-window-content">
-              <img src="" className="importImage" alt="导入图片" />
-              <input type="file" className="importFile" onChange={onImportFile} />
-            </div>
-          </main>
+      <div className="setting-window">
+        <div className="setting-window-item" onClick={onSettingBgOpen}>
+          壁纸设置
         </div>
-      ) : (
-        <div></div>
-      )}
+        <div className="setting-window-item">关于</div>
+      </div>
+      <div className="setting-background disappear">
+        <main className="setting-background-content">
+          <header className="content-title">壁纸图片设置</header>
+          <div className="setting-background-item">
+            <div className="item-title">自定义壁纸</div>
+            <div className="import-wrapper">
+              <img className="import-image" src={defaultImage} alt="导入图片" />
+              <div className="import-text">
+                <div className="import-describe">导入你喜欢的图片设为壁纸</div>
+                <div className="import-describe-sub">建议尺寸：分辨率 1920x1080 或以上</div>
+                <input type="file" className="import-file" onChange={onImportFile} />
+                <button className="import-file-button" onClick={onClickImportFile}>
+                  浏览文件
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="setting-background-item">
+            <div className="item-title">默认壁纸</div>
+            <div className="image-wrapper">
+              <img className="image-item" src={defaultImage} alt="默认壁纸" />
+              <img className="image-item" src={defaultImage} alt="默认壁纸" />
+              <img className="image-item" src={defaultImage} alt="默认壁纸" />
+            </div>
+          </div>
+        </main>
+        <div className="setting-background-close" onClick={onSettingBgClose}>
+          <svg className="icon " aria-hidden="true">
+            <use xlinkHref="#icon-close"></use>
+          </svg>
+        </div>
+      </div>
 
       {/* <div className="star-wrapper">
         <div className="star-surround">
